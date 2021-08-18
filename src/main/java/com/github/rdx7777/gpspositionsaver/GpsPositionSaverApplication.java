f6197dd5e390d6f1c1cb5ae87dfca0c9c6e22156
Radek Jerzynski <47062194+rdx7777@users.noreply.github.com>
2021-08-18 22:58:55 +0100

    First commit.

---
+++ src/main/java/com/github/rdx7777/gpspositionsaver/GpsPositionSaverApplication.java
@@ -0,0 +1,13 @@
+package com.github.rdx7777.gpspositionsaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsPositionSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpsPositionSaverApplication.class, args);
+	}
+
+}
